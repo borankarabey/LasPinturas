b908e867a46ccd2b13909e0eafa03941c885a599
Emre <73621311+borankarabey@users.noreply.github.com>
2022-05-09 14:31:58 +0300

    Web Commit

---
+++ LasPinturas.Web/ViewComponents/CartViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using LasPinturas.Web.Extensions;
+using LasPinturas.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LasPinturas.Web.ViewComponents
+{
+    public class CartViewComponent : ViewComponent
+    {
+
+        public IViewComponentResult Invoke()
+        {
+            var collection = HttpContext.Session.GetJson<CartCollection>("sepetim");
+            return View(collection);
+        }
+    }
+}
